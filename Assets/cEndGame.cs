e2ed982d5b383073b8dbef5236988bf1ac4cf3df
ilagam <magali.hautcoeur@hotmail.fr>
2016-05-27 22:36:48 +0200

    Ajout des scenes

---
+++ Assets/cEndGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cEndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2, 100, 50), "Replay")) {
+			Application.LoadLevel ("ScGame");
+		}
+		GUI.Label (new Rect (Screen.width/2 - 25, Screen.height/2 + 80, 100, 50), "Scores");
+		GUI.Label (new Rect (Screen.width/2 - 25, Screen.height/2 + 120, 100, 50), "123369");
+	}
+		
+}
---
+++ Assets/cMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2, 100, 50), "Play")) {
+			Application.LoadLevel ("ScGame");
+		}
+		GUI.Button (new Rect (Screen.width/2 - 50, Screen.height/2 + 70, 100, 50), "Scores");
+
+	}
+}
