9f44e2a55864571a06b26fee255f6d1b80cecc89
isqo <qouiqaismail@gmail.com>
2016-05-28 22:32:44 +0200

    [ScGame] make the camera move based on bird movement.

---
+++ Assets/Camera/cMoveCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class cMoveCamera : MonoBehaviour
+    {
+        public float lookSensitivity = 2.0f;
+        public float BehindThePlayerZAxis = -2.0f;
+        private float yRotation;
+        private float xRotation;
+
+        void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                yRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+                xRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+                gameObject.transform.rotation = Quaternion.Euler(yRotation, xRotation, BehindThePlayerZAxis);
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, BehindThePlayerZAxis);
+            }
+        }
+    }
+}
