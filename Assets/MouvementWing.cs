6cc3f0eb0e4aa05ed0489413377abde35fc17eec
ilagam <magali.hautcoeur@hotmail.fr>
2016-05-28 20:28:11 +0200

    Mouvement des ailles

---
+++ Assets/MouvementWing.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.Timers;
+using System;
+
+public class MouvementWing2 : MonoBehaviour {
+
+        float z = 30f;
+        public Timer timer1;
+        public bool flag = false;
+        public bool test = false;
+        // Use this for initialization
+        void Start()
+        {
+            timer1 = new Timer();
+            timer1.Elapsed += new ElapsedEventHandler(timer1_Tick);
+            timer1.Interval = 1000; // in miliseconds
+            timer1.Start();
+        }
+	
+	// Update is called once per frame
+	void Update () {
+        if (flag)
+        {
+
+            if (test)
+            {
+                transform.Rotate(new Vector3(0.0f, 0.0f, z));
+                test = false;
+
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0.0f, 0.0f, -z));
+                test = true;
+
+            }
+            flag = false;
+        }
+
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        flag = true;
+    }
+}
+
