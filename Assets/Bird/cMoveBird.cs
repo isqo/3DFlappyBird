d595241bfc27c71e8ad216d2febf1439aaae6296
isqo <qouiqaismail@gmail.com>
2016-05-28 22:51:31 +0200

    [ScGame] add the movement and the jumping ability to the bird.

---
+++ Assets/Bird/cMoveBird.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cMoveBird : MonoBehaviour {
+    public float Speed=5;
+    public float Yvelocity = 12.0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+            GetComponent<Rigidbody>().velocity = new Vector3(0, Yvelocity, 0);
+
+        transform.Translate(0, 0, Time.deltaTime * Speed);
+    }
+}
