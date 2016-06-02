f15c593efd639878c921842325cab4009a3ecd05
isqo <qouiqaismail@gmail.com>
2016-06-02 18:08:44 +0200

    [Scgame] the bird stops when it's colliding with a pipe.

--- Assets/Bird/cMoveBird.cs
+++ Assets/Bird/cMoveBird.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class cMoveBird : MonoBehaviour {
     public float Speed=5;
     public float Yvelocity = 12.0f;
+    public static bool CollisionFlag = false;
 
     // Use this for initialization
     void Start () {
@@ -11,9 +12,13 @@ public class cMoveBird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("space"))
-            GetComponent<Rigidbody>().velocity = new Vector3(0, Yvelocity, 0);
 
-        transform.Translate(0, 0, Time.deltaTime * Speed);
+        if (!CollisionFlag)
+        {
+            if (Input.GetKey("space"))
+                GetComponent<Rigidbody>().velocity = new Vector3(0, Yvelocity, 0);
+
+            transform.Translate(0, 0, Time.deltaTime * Speed);
+        }
     }
 }
