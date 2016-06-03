a69b8de71b3b93733efb7e2a66337b316c80a6ad
isqo <qouiqaismail@gmail.com>
2016-06-03 13:19:08 +0200

    [Scgame] add the levels.

---
+++ Assets/Levels/Level1/level1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Levels.Level1
+{
+    public class level1
+    {
+        public static bool isActif;
+
+        public level1()
+        {
+            Physics.gravity = new Vector3(0, -19, 0);
+        }
+
+    }
+}
---
+++ Assets/Levels/Level3/level3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Levels.Level3
+{
+    public class level3
+    {
+        public static bool isActif;
+
+        public level3()
+        {
+            Physics.gravity = new Vector3(0, -35, 0);
+        }
+
+    }
+}
